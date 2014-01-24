7bc324362955dae58065211e7b1c05bdc690856c
silverriaz <silverriaz@gmail.com>
2014-01-23 23:37:09 -0500

    Updated like tags for predictable behavior

--- app/groups/templatetags/like_tags.py
+++ app/groups/templatetags/like_tags.py
@@ -1,6 +1,8 @@
+from django.core.urlresolvers import reverse
 from django import template
+
+
 register = template.Library()
-import pdb;
 
 @register.filter 
 def pdb(element):
@@ -8,7 +10,7 @@ def pdb(element):
     return element
 
 @register.simple_tag 
-def render_likes(likedList, loggedUser):
+def render_likes(likedList, loggedUser, obj):
     """
     This tag is used to render likes in a readable format
     For example: 
@@ -18,33 +20,45 @@ def render_likes(likedList, loggedUser):
     X likes this message
     X and Y like this message
     X, Y and Z like this message
-    
+    Like link should prevent auto clicks.
+    What if javascript is disabled 
     """
     total_likes = likedList.count()
-    liked_by = ''
+    liked_by = '<span style="border:1px solid black;width:auto;" >'
+    
+    #To change when 2nd person singular happens 
     like = "like"
+    like_link = '';
     #check if current user likes this message
     if loggedUser in [ likeobj.user for likeobj in likedList]:
-        liked_by = 'You'
-    elif total_likes == 1:
+        liked_by += 'You'
+    else:
+        #set the link url
+        like_link = '<span><a href="'+reverse('group-discussion-message-like',args=(obj.id,))+'">Like<a/>. </span>'
+        liked_by += like_link
+        #Only one person liked and its not me, so change to likes
+        if total_likes == 1:
             like = "likes"
-        #remove current user from the list
-        #@todo: after establishing only one like per user
-        #use del. It's more efficient
-        #likedList = [ a for a in likedList if a.user !=loggedUser ]
-        
-        #@todo: ok filter lambda is not working
     
+    #remove current user from the list since You is established
+    #@todo: after establishing only one like per user
+    #use del. It's more efficient
+    #@todo: ok filter lambda is not working
+    likedList = [ a for a in likedList if a.user !=loggedUser ]
+        
     if total_likes > 1:    
         for i in range(len(likedList)):
-            #if likedList[i].user != loggedUser:
             if i == len(likedList)-1: 
                 liked_by += ' and '+str(likedList[i].user)
             else:
                 liked_by += ', '+str(likedList[i].user)
-    
+
     if total_likes > 0:
         liked_by += " "+like+" this message."
-               
-    return "( Total "+ str(total_likes) +" Likes ) "+liked_by;
+    liked_by += like.title()+" ("+ str(total_likes) +")"
+    
+    liked_by += '</span>'
+    #<a href="{% url 'group-discussion-message-like' messageobj.id %}">Like<a/> ({{likes|length}} Likes)
+        
+    return liked_by;
 
